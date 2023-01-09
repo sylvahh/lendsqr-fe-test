0d7c0059b744816de94ef1c57ad147260dccd469
sylvahh <sylvesterchambers03@gmail.com>
2023-01-09 14:39:21 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.scss';
+import Login from './views/login/Login';
+
+function App() {
+  return (
+
+    <div className="">
+
+      
+      <Login/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/login/Login.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+// import '../../sass/layout/_loginPage.scss'
+import logo from '../../assets/Union.png';
+import logoText from '../../assets/lendsqr.svg';
+import welcomeImg from '../../assets/pablo-sign-in 1.png';
+
+const Login = () => {
+  return (
+    <div id='login_page' className=''>
+      <div className='logo_Wraper'>
+        <img className='logo' src={logo} alt='logo' />
+        <img className='logo_text' src={logoText} alt='logoText' />
+      </div>
+
+      <div className='login_form_wraper'>
+        <div className='welcome_img_wraper'>
+          
+        <img className='welcome_Img' src={welcomeImg} alt='welcomeImg' />
+       </div>
+
+        <div className='form_wraper'>
+          <h1>Welcome!</h1>
+          <span>Enter details to login.</span>
+          <form action=''>
+            <div>
+              <input type='text' placeholder='Email' />
+            </div>
+            <div className='form_password'>
+              <input type='password' placeholder='Password' />
+              <button className='showPassbtn'>SHOW</button>
+            </div>
+            <button className='reset_password'>forgot password ?</button >
+
+            <button className='loginBtn'> log in </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
