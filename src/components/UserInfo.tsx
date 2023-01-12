cc449b5b93715992ae92673f5fc0124de1a73194
sylvahh <sylvesterchambers03@gmail.com>
2023-01-12 14:30:35 +0100

    added user info

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,9 +1,135 @@
-import React from 'react'
+import React from 'react';
 
 const UserInfo = () => {
   return (
-    <div>UserInfo</div>
-  )
-}
+    <div id='user-info'>
+      <div className='user-info_wraper'>
+        <h1>Personal Information</h1>
+        <div className='user-info_section-wraper'>
+          <div>
+            <h1>full name</h1>
+            <span>Grace Effiom</span>
+          </div>
+          <div>
+            <h1>Phone Number</h1>
+            <span>07060780922</span>
+          </div>{' '}
+          <div>
+            <h1>Email Address</h1>
+            <span>grace@gmail.com</span>
+          </div>{' '}
+          <div>
+            <h1>Bvn</h1>
+            <span>07060780922</span>
+          </div>{' '}
+          <div>
+            <h1>gender</h1>
+            <span>female</span>
+          </div>{' '}
+          <div>
+            <h1>Marital status</h1>
+            <span>Single </span>
+          </div>{' '}
+          <div>
+            <h1>Children</h1>
+            <span>none</span>
+          </div>{' '}
+          <div>
+            <h1>Type of residence None</h1>
+            <span>Parent’s Apartment</span>
+          </div>
+        </div>
+        <hr />
+        <h1>Education and Employment</h1>
+        <div className='user-info_section-wraper'>
+          <div>
+            <h1> level of education</h1>
+            <span>B.Sc</span>
+          </div>{' '}
+          <div>
+            <h1>employment status </h1>
+            <span>Employed</span>
+          </div>
+          <div>
+            <h1>sector of employment </h1>
+            <span>FinTech</span>
+          </div>{' '}
+          <div>
+            <h1> office email</h1>
+            <span>grace@lendsqr.com</span>
+          </div>{' '}
+          <div>
+            <h1>Duration of employment </h1>
+            <span>2 years</span>
+          </div>{' '}
+          <div>
+            <h1> Monthly income</h1>
+            <span>₦200,000.00- ₦400,000.00</span>
+          </div>{' '}
+          <div>
+            <h1> loan repayment</h1>
+            <span>40,000 </span>
+          </div>{' '}
+        </div>
+        <hr />
+        <h1>socials</h1>
+        <div className='user-info_section-wraper'>
+          <div>
+            <h1> Twitter</h1>
+            <span>@grace_effiom</span>
+          </div>
+          <div>
+            <h1>Facebook </h1>
+            <span>Grace Effiom</span>
+          </div>{' '}
+          <div>
+            <h1> Instagram</h1>
+            <span>@grace_effiom</span>
+          </div>{' '}
+        </div>
+      <hr />
+      <h1>Guarantor</h1>
+      <div className='user-info_section-wraper'>
+        <div>
+          <h1>full Name </h1>
+          <span>Debby Ogana</span>
+        </div>
+        <div>
+          <h1> Phone Number</h1>
+          <span>07060780922</span>
+        </div>{' '}
+        <div>
+          <h1>Email Address </h1>
+          <span>debby@gmail.com</span>
+        </div>{' '}
+        <div>
+          <h1> Relationship</h1>
+          <span>Sister</span>
+        </div>{' '}
+      </div>
 
-export default UserInfo
+      <hr />
+      <div className='user-info_section-wraper'>
+        <div>
+          <h1>full Name </h1>
+          <span>Debby Ogana</span>
+        </div>
+        <div>
+          <h1> Phone Number</h1>
+          <span>07060780922</span>
+        </div>{' '}
+        <div>
+          <h1>Email Address </h1>
+          <span>debby@gmail.com</span>
+        </div>{' '}
+        <div>
+          <h1> Relationship</h1>
+          <span>Sister</span>
+        </div>{' '}
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
